0f0d4c6011c724a925cab6b934c11379c5c99b73
vidya <16vidya98@gmail.com>
2020-07-08 17:20:05 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,129 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geoCode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+// console.log(__dirname)
+// console.log(path.join(__dirname, '../public'))
+
+const app = express()
+
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebar engines and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Vidya'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Vidya'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        content: 'Help coming soon!!',
+        title: 'Help',
+        name: 'Vidya'
+    })
+})
+
+
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'Please provide an address'
+        })
+
+    }
+
+    geoCode(req.query.address, (error, { latitude, longitude, location } = {}) =>{
+        if(error){
+           return res.send({
+               error
+           })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return res.send({
+                    error
+                })
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+           
+        })
+        
+    })
+})
+
+app.get('/products',(req, res) => {
+    if(!req.query.search){
+       return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    console.log(req.query)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('error', {
+        title: '404 help',
+        name: 'Vidya',
+        underViews: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('error', {
+        title: '404',
+        name: 'Vidya',
+        underViews: 'Page not found'
+    })
+})
+
+app.listen(3000, () =>{
+    console.log('Server is up on port 3000')
+})
+
+
+
+
+
+// app.get('', (req, res) => {
+//     res.send('<h1>Weather</h1>')
+// })
+
+// app.get('/help', (req, res) => {
+//     res.send({
+//         name: 'vidya',
+//         age: 22
+//     })
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About page</h1>')
+// })
